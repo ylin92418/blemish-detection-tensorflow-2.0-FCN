aa76ddd5eceb81b8abe1e05e8cdf637092b37415
Y Lin <linyeah2011@gmail.com>
2021-02-10 19:52:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,33 +7,33 @@ Created on Oct 20 2020
 
 from easydict import EasyDict as edict
 
-_C = edict()
-cfg = _C
+_CFG = edict()
+cfg = _CFG
 
 
 # Config. for training
 # Retrieve value by cfg.TRAIN.XXXX
 
-_C.TRAIN                    = edict() 
+_CFG.TRAIN                    = edict() 
 
-_C.TRAIN.IMG_SIZE           = (224,224)
-_C.TRAIN.BATCH_SIZE         = 5
-_C.TRAIN.EPOCHS             = 60  
-_C.TRAIN.FCN_TYPE           = 2    
-_C.TRAIN.N_CLASSES          = 2 
-_C.TRAIN.OPTIMIZER          = 0 # 0: Adelta, 1: Adam 
-_C.TRAIN.DATASET            = ['./dataset/FCN_500_imageset', './dataset/FCN_imageset'] 
-_C.TRAIN.LABELSET           = ['./dataset/FCN_500_refine_txt', './dataset/FCN_refine_txt'] 
+_CFG.TRAIN.IMG_SIZE           = (224,224)
+_CFG.TRAIN.BATCH_SIZE         = 5
+_CFG.TRAIN.EPOCHS             = 60  
+_CFG.TRAIN.FCN_TYPE           = 2    
+_CFG.TRAIN.N_CLASSES          = 2 
+_CFG.TRAIN.OPTIMIZER          = 0 # 0: Adelta, 1: Adam 
+_CFG.TRAIN.DATASET            = ['./dataset/FCN_500_imageset', './dataset/FCN_imageset'] 
+_CFG.TRAIN.LABELSET           = ['./dataset/FCN_500_refine_txt', './dataset/FCN_refine_txt'] 
 
 # Config. for evaluation
 # Retrieve value by cfg.EVAL.XXXX
 
-_C.EVAL                     = edict() 
+_CFG.EVAL                     = edict() 
 
-_C.EVAL.USE_CPU             = True
-_C.EVAL.INPUT_SIZE          = 320
-_C.EVAL.FIRST_LAYER_SIZE    = 320
-_C.EVAL.FCN_TYPE            = 2  
-_C.EVAL.N_CLASSES           = 2 
-_C.EVAL.THRESHOLD           = 0.1
-_C.EVAL.MAX_BATCH_SIZE      = 2
+_CFG.EVAL.USE_CPU             = True
+_CFG.EVAL.INPUT_SIZE          = 320
+_CFG.EVAL.FIRST_LAYER_SIZE    = 320
+_CFG.EVAL.FCN_TYPE            = 2  
+_CFG.EVAL.N_CLASSES           = 2 
+_CFG.EVAL.THRESHOLD           = 0.1
+_CFG.EVAL.MAX_BATCH_SIZE      = 2
